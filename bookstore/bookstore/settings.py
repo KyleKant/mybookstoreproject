5e244488b1288915bee8d251720ff01463429ba0
Kylekant <onhanam1234@gmail.com>
2023-02-22 16:02:22 +0700

    deploy zeet

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(e^$*-=)lpy$xl1s1pq27j1&pv2p*b@y@od@*1sd=1%qgx29c$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
